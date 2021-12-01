b0181125dfe364db73c556cd0c1a4b3f81c48ac8
David Lang <davidlang42@gmail.com>
2021-12-02 08:52:23 +1100

    Update for part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -9,21 +9,25 @@ fn main() {
             .expect(&format!("Error reading from {}", filename));
         let numbers: Vec<u32> = text.split("\r\n").map(|s| s.parse()
             .expect(&format!("Error parsing number {}", s))).collect();
-        let result = count_increasing(numbers);
-        println!("Result: {}", result);
+        println!("Increasing by 1: {}", count_increasing(&numbers, 1));
+        println!("Increasing by 3: {}", count_increasing(&numbers, 3));
     } else {
         println!("Please provide 1 argument: Filename");
     }
 }
 
-fn count_increasing(list: Vec<u32>) -> u32 {
-    let mut previous: u32 = 0;
+fn count_increasing(list: &Vec<u32>, by: usize) -> u32 {
     let mut count: u32 = 0;
-    for (_, value) in list.iter().enumerate() {
-        if value > &previous {
+    let mut previous: u32 = 0;
+    for i in by-1..list.len() {
+        let mut value: u32 = 0;
+        for offset in 0..by {
+            value += list[i-offset];
+        }
+        if value > previous {
             count += 1;
         }
-        previous = *value;
+        previous = value;
     }
     return count - 1;
 }
