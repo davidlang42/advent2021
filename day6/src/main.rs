2f3afa97c47718c195a1acb0831ed5307b20c656
David Lang <davidlang42@gmail.com>
2023-01-04 16:33:43 +1100

    day6 part1

---
+++ day6/src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let filename = &args[1];
+        let text = fs::read_to_string(&filename)
+            .expect(&format!("Error reading from {}", filename));
+        let mut fish: Vec<isize> = text.split(",").map(|n| n.parse().unwrap()).collect();
+        for _ in 0..80 {
+            let mut new_fish = simulate(&mut fish);
+            fish.append(&mut new_fish);
+        }
+        println!("Fish: {}", fish.len());
+    } else {
+        println!("Please provide 1 argument: Filename");
+    }
+}
+
+fn simulate(fish: &mut Vec<isize>) -> Vec<isize> {
+    let mut new_fish = Vec::new();
+    for i in 0..fish.len() {
+        if fish[i] == 0 {
+            new_fish.push(8);
+            fish[i] = 6;
+        } else {
+            fish[i] -= 1;
+        }
+    }
+    new_fish
+}
