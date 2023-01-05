27a093423159cc8252901474c6c0820f7fe630ef
David Lang <davidlang42@gmail.com>
2023-01-05 17:35:39 +1100

    part2 wip

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -10,7 +10,13 @@ struct Entry {
 #[derive(Debug, PartialEq, Eq)]
 struct Signal([bool; 7]);
 
-//struct WireMap([usize; 7]);
+struct WireMap([PartialMap; 7]);
+
+#[derive(Copy, Clone)]
+enum PartialMap {
+    Definite(usize),
+    CouldBe([bool; 7])
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,14 +25,52 @@ fn main() {
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let entries: Vec<Entry> = text.lines().map(|l| l.parse().unwrap()).collect();
-        let count1478 = entries.iter().flat_map(|e| e.output.iter()).filter(|s| match s.segments() {
+        let count1478 = entries.iter().flat_map(|e| e.output.iter()).filter(|s| match s.segments().len() {
             2 => true, // "1"
-            3 => true, // "4"
-            4 => true, // "7"
+            4 => true, // "4"
+            3 => true, // "7"
             7 => true, // "8"
             _ => false
         }).count();
         println!("Simple digits: {}", count1478);
+        let digits = [
+            Signal::from_str("abcefg").unwrap(),
+            Signal::from_str("cf").unwrap(),
+            Signal::from_str("acdeg").unwrap(),
+            Signal::from_str("acdfg").unwrap(),
+            Signal::from_str("bdcf").unwrap(),
+            Signal::from_str("abdfg").unwrap(),
+            Signal::from_str("abdefg").unwrap(),
+            Signal::from_str("acf").unwrap(),
+            Signal::from_str("abcdefg").unwrap(),
+            Signal::from_str("abcdfg").unwrap()
+        ];
+        let mut sum = 0;
+        for entry in entries {
+            let mut wire_map = WireMap::new(); //TODO &entry.unique, &digits);
+            for unique in &entry.unique {
+                let easy_match = match unique.segments().len() {
+                    2 => Some(&digits[1]), // "1"
+                    4 => Some(&digits[4]), // "4"
+                    3 => Some(&digits[7]), // "7"
+                    7 => Some(&digits[8]), // "8"
+                    _ => None
+                };
+                if let Some(actual) = easy_match {
+                    wire_map.map(unique, actual);
+                }
+            } 
+            let mut output = 0;
+            for raw in &entry.output {
+                let decoded = raw.decode(&wire_map);
+                let number = digits.iter().position(|d| *d == decoded).unwrap();
+                output *= 10;
+                output += number;
+            }
+            println!("Output number: {}", output);
+            sum += output;
+        }
+        println!("Sum of output numbers: {}", sum);
     } else {
         println!("Please provide 1 argument: Filename");
     }
@@ -73,21 +117,81 @@ impl FromStr for Signal {
 }
 
 impl Signal {
-    // fn decode(&self, map: &WireMap) -> Signal {
-    //     let mut signal = [false; 7];
-    //     for i in 0..7 {
-    //         signal[i] = self[map[i]];// this might be backwards?
-    //     }
-    //     Signal(signal)
-    // }
-
-    fn segments(&self) -> usize {
-        let mut count = 0;
+    fn decode(&self, map: &WireMap) -> Signal {
+        let mut signal = [false; 7];
         for i in 0..7 {
-            if self.0[i] {
-                count += 1;
+            if let PartialMap::Definite(new_i) = map.0[i] {
+                signal[i] = self.0[new_i]; //TODO this might be backwards?
+            } else {
+                panic!("Cannot decode with an incomplete map");
             }
         }
-        count
+        Signal(signal)
+    }
+
+    fn segments(&self) -> Vec<usize> {
+        let mut seggs = Vec::new();
+        for i in 0..7 {
+            if self.0[i] {
+                seggs.push(i);
+            }
+        }
+        seggs
+    }
+
+    // fn could_be(&self, map: &WireMap) -> bool {
+
+    // }
+}
+
+impl WireMap {
+    fn new() -> Self {
+        WireMap([PartialMap::CouldBe([true; 7]); 7])
+    }
+
+    fn map(&mut self, input_signal: &Signal, output_signal: &Signal) {
+        let mut inputs = input_signal.segments();
+        let mut outputs = output_signal.segments();
+        if inputs.len() != outputs.len() {
+            panic!("Cannot map signals with different segment counts");
+        }
+        // remove any already mapped inputs and their respective output
+        let mut i = 0;
+        while i < inputs.len() {
+            let input = inputs[i];
+            if let PartialMap::Definite(output) = self.0[input] {
+                if let Some(pos) = outputs.iter().position(|o| *o == output) {
+                    outputs.remove(pos);
+                    inputs.remove(i);
+                } else {
+                    panic!("Invalid mapping");
+                }
+            } else {
+                i += 1;
+            }
+        }
+        // mark each remaining input as only possibly mapping to any of the remaining outputs
+        for input in inputs {
+            if let PartialMap::CouldBe(old) = self.0[input] {
+                let mut new = [false; 7];
+                let mut remaining = Vec::new();
+                for output in &outputs {
+                    if old[*output] {
+                        // this 'input' could still map to this 'output'
+                        new[*output] = true;
+                        remaining.push(output);
+                    }
+                }
+                self.0[input] = if remaining.len() == 0 {
+                    panic!("No options remaining");
+                } else if remaining.len() == 1 {
+                    PartialMap::Definite(*remaining[0])
+                } else {
+                    PartialMap::CouldBe(new)
+                };
+            } else {
+                panic!("Definite found while updating CouldBe's");
+            }
+        }
     }
 }
