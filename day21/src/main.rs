ae964aae4a83192dd1ef3d2a1abe73873eb29c84
David Lang <davidlang42@gmail.com>
2023-01-18 17:18:39 +1100

    part2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::fs;
 use std::str::FromStr;
+use std::collections::HashMap;
 
+#[derive(Clone, Hash, Eq, PartialEq)]
 struct Player {
     position: usize,
     score: usize
@@ -28,6 +30,22 @@ impl DeterministicDie {
         } else {
             value
         }
+    }
+}
+
+struct DiracDie {
+    sides: usize
+}
+
+impl DiracDie {
+    fn new(sides: usize) -> Self {
+        Self {
+            sides
+        }
+    }
+
+    fn roll(&self) -> Vec<usize> {
+        (1..(self.sides+1)).collect()
     }
 }
 
@@ -64,6 +82,21 @@ impl Player {
             new_pos
         };
     }
+
+    fn play_dirac(&self, die: &DiracDie) -> Vec<Player> {
+        let mut result = Vec::new();
+        for d1 in die.roll() {
+            for d2 in die.roll() {
+                for d3 in die.roll() {
+                    let mut copy = self.clone();
+                    copy.advance(d1 + d2 + d3);
+                    copy.score += copy.position;
+                    result.push(copy);
+                }
+            }
+        }
+        result
+    }
 }
 
 impl FromStr for Player {
@@ -81,18 +114,126 @@ fn main() {
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let mut players: Vec<Player> = text.lines().map(|l| l.parse().unwrap()).collect();
-        let mut die = DeterministicDie::new(100);
-        loop {
-            for i in 0..players.len() {
-                if let PlayResult::Winner(score) = players[i].play(&mut die) {
-                    println!("Player {} wins with {} points", i+1, score);
-                    let lowest = players.iter().map(|p| p.score).min().unwrap();
-                    println!("Lowest score * Rolls = {} * {} = {}", lowest, die.rolls, lowest * die.rolls);
-                    return;
+        run_deterministic(players);
+        players = text.lines().map(|l| l.parse().unwrap()).collect();
+        run_dirac(players);
+    } else {
+        println!("Please provide 1 argument: Filename");
+    }
+}
+
+fn run_deterministic(mut players: Vec<Player>) {
+    let mut die = DeterministicDie::new(100);
+    loop {
+        for i in 0..players.len() {
+            if let PlayResult::Winner(score) = players[i].play(&mut die) {
+                println!("Player {} wins with {} points", i+1, score);
+                let lowest = players.iter().map(|p| p.score).min().unwrap();
+                println!("Lowest score * Rolls = {} * {} = {}", lowest, die.rolls, lowest * die.rolls);
+                return;
+            }
+        }
+    }
+}
+
+fn run_dirac(players: Vec<Player>) {
+    let die = DiracDie::new(3);
+    let universe = Universe::new(players);
+    let wins = universe.simulate(&die);
+    for (i, wins) in wins.0.iter() {
+        println!("Player {} wins in {} universes", i+1, wins);
+    }
+    println!("Most wins: {}", wins.most());
+}
+
+struct Wins(HashMap<usize, usize>);
+
+impl Wins {
+    fn new() -> Self {
+        Wins(HashMap::new())
+    }
+
+    fn increment(&mut self, winner: usize, count: usize) {
+        match self.0.get(&winner) {
+            Some(existing) => self.0.insert(winner, existing + count),
+            None => self.0.insert(winner, count)
+        };
+    }
+
+    fn most(&self) -> usize {
+        *self.0.values().max().unwrap()
+    }
+}
+
+#[derive(Eq, Hash, PartialEq, Clone)]
+struct Universe {
+    players: Vec<Player>,
+    next_player: usize
+}
+
+struct Multiverse {
+    universes: HashMap<Universe, usize>
+}
+
+impl Multiverse {
+    fn new() -> Self {
+        Self {
+            universes: HashMap::new()
+        }
+    }
+
+    fn push(&mut self, universe: Universe, count: usize) {
+        match self.universes.get(&universe) {
+            Some(existing) => self.universes.insert(universe, existing + count),
+            None => self.universes.insert(universe, count)
+        };
+    }
+
+    fn pop(&mut self) -> (Universe, usize) {
+        let key = self.universes.keys().next().unwrap();
+        let uni = key.clone();
+        let count = self.universes.remove(&uni).unwrap();
+        (uni, count)
+    }
+
+    fn len(&self) -> usize {
+        self.universes.len()
+    }
+}
+
+impl Universe {
+    fn new(players: Vec<Player>) -> Self {
+        Self {
+            players,
+            next_player: 0
+        }
+    }
+
+    fn simulate(self, die: &DiracDie) -> Wins {
+        let mut multi = Multiverse::new();
+        multi.push(self, 1);
+        let mut wins = Wins::new();
+        while multi.len() > 0 {
+            let (uni, count) = multi.pop();
+            for new_player in uni.players[uni.next_player].play_dirac(die) {
+                if new_player.score >= 21 {
+                    wins.increment(uni.next_player, count);
+                } else {
+                    let new_uni = uni.split(new_player);
+                    multi.push(new_uni, count);
                 }
             }
         }
-    } else {
-        println!("Please provide 1 argument: Filename");
+        wins
+    }
+
+    fn split(&self, replacement_player: Player) -> Universe {
+        let mut new_players = self.players.clone();
+        new_players[self.next_player] = replacement_player;
+        let new_next_player = (self.next_player + 1) % self.players.len();
+        Self {
+            players: new_players,
+            next_player: new_next_player
+        }
     }
 }
