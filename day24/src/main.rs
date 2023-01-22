74d2bee5a8ca234ddcce4b5b0a62650ee2cbe5d8
David Lang <davidlang42@gmail.com>
2023-01-22 19:53:12 +1100

    Revert "refactor to expression based"
    This reverts commit e98fd42d4bdc55a298df60fa4ec235f21cf31436.

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,14 +1,30 @@
 use std::env;
 use std::fs;
 use std::str::FromStr;
+use std::collections::VecDeque;
 use std::collections::HashMap;
 
-#[derive(Eq, Hash, PartialEq, Copy, Clone)]
+struct ArithmeticLogicUnit {
+    variables: HashMap<Variable, isize>,
+    inputs: VecDeque<isize>
+}
+
+#[derive(Eq, Hash, PartialEq, Clone)]
 enum Variable {
     W,
     X,
     Y,
     Z
+}
+
+enum Instruction {
+    Input(Variable),
+    Operation(Variable, Operator, Expression)
+}
+
+enum Expression {
+    Variable(Variable),
+    Literal(isize)
 }
 
 enum Operator {
@@ -19,10 +35,64 @@ enum Operator {
     Equal
 }
 
-enum Instruction {
-    Input(Variable),
-    VariableOperation(Variable, Operator, Variable),
-    LiteralOperation(Variable, Operator, isize)
+impl Operator {
+    fn operate(&self, a: isize, b: isize) -> isize {
+        match self {
+            Self::Add => a + b,
+            Self::Multiply => a * b,
+            Self::Divide => {
+                if b == 0 {
+                    panic!("b cannot be zero in a div operation");
+                }
+                a / b
+            },
+            Self::Modulo => {
+                if a < 0 {
+                    panic!("a cannot be less than zero in a mod operation");
+                }
+                if b <= 0 {
+                    panic!("b cannot be less than or equal to zero in a mod operation");
+                }
+                a % b // negative numbers are disallowed
+            },
+            Self::Equal => if a == b { 1 } else { 0 }
+        }
+    }
+}
+
+impl ArithmeticLogicUnit {
+    fn new(inputs: VecDeque<isize>) -> Self {
+        Self {
+            variables: HashMap::new(),
+            inputs
+        }
+    }
+
+    fn get(&self, var: &Variable) -> isize {
+        *self.variables.get(var).unwrap_or(&0)
+    }
+
+    fn set(&mut self, var: Variable, value: isize) {
+        self.variables.insert(var, value);
+    }
+
+    fn run(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::Input(var) => {
+                let new_value = self.inputs.pop_front().expect("Ran out of input values");
+                self.set(var.clone(), new_value);
+            },
+            Instruction::Operation(var, op, exp) => {
+                let a = self.get(var);
+                let b = match exp {
+                    Expression::Variable(b_var) => self.get(b_var),
+                    Expression::Literal(b_literal) => *b_literal
+                };
+                let new_value = op.operate(a, b);
+                self.set(var.clone(), new_value);
+            }
+        }
+    }
 }
 
 impl FromStr for Variable {
@@ -36,6 +106,17 @@ impl FromStr for Variable {
             "z" => Ok(Self::Z),
             _ => Err(format!("Invalid variable: {}", letter))
         }
+    }
+}
+
+impl FromStr for Expression {
+    type Err = String;
+
+    fn from_str(expression: &str) -> Result<Self, Self::Err> {
+        Ok(match expression.parse::<Variable>() {
+            Ok(var) => Expression::Variable(var),
+            Err(_) => Expression::Literal(expression.parse::<isize>().map_err(|e| e.to_string())?)
+        })
     }
 }
 
@@ -61,19 +142,15 @@ impl FromStr for Instruction {
         let words: Vec<&str> = line.split(" ").collect();
         match (words[0], words.len() - 1) {
             ("inp", 1) => Ok(Self::Input(words[1].parse::<Variable>().map_err(|e| e.to_string())?)),
-            (op, 2) => {
-                let var1 = words[1].parse::<Variable>().map_err(|e| e.to_string())?;
-                Ok(match words[2].parse::<Variable>() {
-                    Ok(var2) => Self::VariableOperation(var1, op.parse()?, var2),
-                    Err(_) => Self::LiteralOperation(var1, op.parse()?, words[2].parse::<isize>().map_err(|e| e.to_string())?)
-                })
-            },
+            (op, 2) => Ok(Self::Operation(
+                words[1].parse::<Variable>().map_err(|e| e.to_string())?,
+                op.parse()?,
+                words[2].parse::<Expression>().map_err(|e| e.to_string())?,
+            )),
             _ => Err(format!("Invalid instruction: {}", line))
         }
     }
 }
-
-// --------------------------------------------------------------------------------------------------------------
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -82,108 +159,15 @@ fn main() {
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let instructions: Vec<Instruction> = text.lines().map(|l| l.parse().unwrap()).collect();
-        let mut alu = ArithmeticLogicUnit::new();
-        for (i, instruction) in instructions.iter().enumerate() {
-            println!("{}", i);
+        let test_input = "13579246899999";
+        let inputs: VecDeque<isize> = test_input.chars().map(|c| c.to_digit(10).unwrap() as isize).collect();
+        let mut alu = ArithmeticLogicUnit::new(inputs);
+        for instruction in &instructions {
             alu.run(instruction);
         }
-        println!("Z = {:?}", alu.get(&Variable::Z));
+        let valid = alu.get(&Variable::Z) == 0;
+        println!("{} is {} model number", test_input, if valid { "a VALID" } else { "an INVALID "});
     } else {
         println!("Please provide 1 argument: Filename");
-    }
-}
-
-// --------------------------------------------------------------------------------------------------------------
-
-struct ArithmeticLogicUnit {
-    variables: HashMap<Variable, Expression>,
-    input_counter: usize
-}
-
-#[derive(Clone, Debug)]
-enum Expression {
-    Literal(isize),
-    Input(usize),
-    Sum(Vec<Expression>),
-    Product(Vec<Expression>),
-    Division(Box<Expression>, Box<Expression>),
-    Modulo(Box<Expression>, Box<Expression>),
-    Equal(Box<Expression>, Box<Expression>)
-}
-
-impl ArithmeticLogicUnit {
-    fn new() -> Self {
-        Self {
-            variables: HashMap::new(),
-            input_counter: 0
-        }
-    }
-
-    fn get(&self, var: &Variable) -> Expression {
-        if let Some(exp) = self.variables.get(var) {
-            exp.clone()
-        } else {
-            Expression::Literal(0)
-        }
-    }
-
-    fn set(&mut self, var: &Variable, exp: Expression) {
-        self.variables.insert(*var, exp);
-    }
-
-    fn run(&mut self, instruction: &Instruction) {
-        match instruction {
-            Instruction::Input(var) => {
-                self.set(var, Expression::Input(self.input_counter));
-                self.input_counter += 1;
-            },
-            Instruction::VariableOperation(var1, op, var2) => {
-                let new_value = op.operate(self.get(var1), self.get(var2));
-                self.set(var1, new_value);
-            },
-            Instruction::LiteralOperation(var1, op, literal) => {
-                let new_value = op.operate(self.get(var1), Expression::Literal(*literal));
-                self.set(var1, new_value);
-            }
-        }
-    }
-}
-
-impl Operator {
-    fn operate(&self, a: Expression, b: Expression) -> Expression {
-        //TODO simplify
-        match self {
-            Self::Add => {
-                let mut sum: Vec<Expression> = Vec::new();
-                if let Expression::Sum(mut a_terms) = a {
-                    sum.append(&mut a_terms);
-                } else {
-                    sum.push(a);
-                }
-                if let Expression::Sum(mut b_terms) = b {
-                    sum.append(&mut b_terms);
-                } else {
-                    sum.push(b);
-                }
-                Expression::Sum(sum)
-            },
-            Self::Multiply => {
-                let mut product: Vec<Expression> = Vec::new();
-                if let Expression::Product(mut a_terms) = a {
-                    product.append(&mut a_terms);
-                } else {
-                    product.push(a);
-                }
-                if let Expression::Product(mut b_terms) = b {
-                    product.append(&mut b_terms);
-                } else {
-                    product.push(b);
-                }
-                Expression::Product(product)
-            },
-            Self::Divide => Expression::Division(Box::new(a), Box::new(b)),
-            Self::Modulo => Expression::Modulo(Box::new(a), Box::new(b)),
-            Self::Equal => Expression::Equal(Box::new(a), Box::new(b)),
-        }
     }
 }
