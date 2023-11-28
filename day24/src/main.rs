889fc76552e6fe9e0af66fc6321b07e06d8ae702
David Lang <david.lang@displayr.com>
2023-11-28 16:49:44 +1100

    Try simplify again

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -7,6 +7,7 @@ mod functions;
 
 use instructions::Variable;
 
+use crate::alu::FunctionalArithmeticLogicUnit;
 use crate::instructions::Instruction;
 use crate::alu::ArithmeticLogicUnit;
 
@@ -18,26 +19,34 @@ fn main() {
             .expect(&format!("Error reading from {}", filename));
         let instructions: Vec<Instruction> = text.lines().map(|l| l.parse().unwrap()).collect();
 
-        let mut count = 0;
-        let factor = 100.0/((9.0_f64).powf(6.0));
-        for a in (1..10).rev() {
-            for b in (1..10).rev() {
-                for c in (1..10).rev() {
-                    for d in (1..10).rev() {
-                        for e in (1..10).rev() {
-                            for f in (1..10).rev() {
-                                for g in (1..10).rev() {
-                                    for h in (1..10).rev() {
-                                        for i in (1..10).rev() {
-                                            for j in (1..10).rev() {
-                                                for k in (1..10).rev() {
-                                                    for l in (1..10).rev() {
-                                                        for m in (1..10).rev() {
-                                                            for n in (1..10).rev() {
-                                                                if test_model_number([a, b, c, d, e, f, g, h, i, j, k, l, m, n], &instructions) {
-                                                                    println!("VALID: {}{}{}{}{}{}{}{}{}{}{}{}{}{}", a, b, c, d, e, f, g, h, i, j, k, l, m, n);
-                                                                    return;
-                                                                }
+        //brute_force(&instructions);
+        simplify(&instructions);
+        
+    } else {
+        println!("Please provide 1 argument: Filename");
+    }
+}
+
+fn brute_force(instructions: &Vec<Instruction>) {
+    let mut count = 0;
+    let factor = 100.0/((9.0_f64).powf(6.0));
+    for a in (1..10).rev() {
+        for b in (1..10).rev() {
+            for c in (1..10).rev() {
+                for d in (1..10).rev() {
+                    for e in (1..10).rev() {
+                        for f in (1..10).rev() {
+                            for g in (1..10).rev() {
+                                for h in (1..10).rev() {
+                                    for i in (1..10).rev() {
+                                        for j in (1..10).rev() {
+                                            for k in (1..10).rev() {
+                                                for l in (1..10).rev() {
+                                                    for m in (1..10).rev() {
+                                                        for n in (1..10).rev() {
+                                                            if test_model_number([a, b, c, d, e, f, g, h, i, j, k, l, m, n], instructions) {
+                                                                println!("VALID: {}{}{}{}{}{}{}{}{}{}{}{}{}{}", a, b, c, d, e, f, g, h, i, j, k, l, m, n);
+                                                                return;
                                                             }
                                                         }
                                                     }
@@ -46,19 +55,16 @@ fn main() {
                                         }
                                     }
                                 }
-                                count += 1;
-                                println!("{:.4}%", (count as f64)*factor);
                             }
+                            count += 1;
+                            println!("{:.4}%", (count as f64)*factor);
                         }
                     }
                 }
             }
         }
-        
-        println!("Not found.");
-    } else {
-        println!("Please provide 1 argument: Filename");
     }
+    println!("Not found.");
 }
 
 fn test_model_number(inputs: [isize; 14], instructions: &Vec<Instruction>) -> bool {
@@ -67,4 +73,16 @@ fn test_model_number(inputs: [isize; 14], instructions: &Vec<Instruction>) -> bo
         alu.run(instruction);
     }
     alu.get(&Variable::Z) == 0
+}
+
+fn simplify(instructions: &Vec<Instruction>) {
+    let mut func_alu = FunctionalArithmeticLogicUnit::new();
+    for (i, instruction) in instructions.iter().enumerate() {
+        func_alu.run(instruction);
+        println!("After functional instruction #{}, Z functional depth is {}", i, func_alu.get(&Variable::Z).depth());
+    }
+    let func = func_alu.get(&Variable::Z);
+    let func_display = format!("{}", func);
+    //println!("Z = {}", func_display);
+    println!("Z = [{}]", func_display.len());
 }
